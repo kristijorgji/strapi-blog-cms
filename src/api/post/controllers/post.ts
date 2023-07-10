02cb1bcd7cc7bcfd36d24f25a96aa1cb733a5be8
Kristi Jorgji <kristijorgji@hotmail.com>
2023-07-10 16:05:03 +0200

    adjust posts find controller to return createdBy as well and remove not used author field
    update author permissions to modify and delete only own posts. Editors can modify and delete all posts

--- src/api/post/controllers/post.ts
+++ src/api/post/controllers/post.ts
@@ -4,4 +4,36 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::post.post');
+export default factories.createCoreController('api::post.post', ({strapi}) => ({
+  /**
+   * a temporary solution that is not very performant until Strapi allows to get
+   * creator data like they did in previous versions
+   * https://github.com/strapi/documentation/issues/762
+   */
+  // @ts-ignore
+  async find(ctx) {
+    const { data, meta } = await super.find(ctx);
+
+    const query = strapi.db.query('api::post.post');
+
+    await Promise.all(
+      data.map(async (item, index) => {
+        const post = await query.findOne({
+          where: {
+            id: item.id,
+          },
+          select: [],
+          populate: ['createdBy'],
+        });
+
+        data[index].attributes.createdBy = {
+          id: post.createdBy.id,
+          firstname: post.createdBy.firstname,
+          lastname: post.createdBy.lastname,
+        };
+      })
+    );
+
+    return { data, meta };
+  },
+}));
